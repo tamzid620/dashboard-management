fa5bf2535346e2d7e29d285bc4830e22e4b65b52
tamzid620 <tamzid620@gmail.com>
2025-08-20 21:15:07 +0600

    coreUrl changed

--- src/Components/Shared/Layout/Layout.jsx
+++ src/Components/Shared/Layout/Layout.jsx
@@ -20,52 +20,52 @@ const Layout = () => {
         location.pathname.includes('pendingStudent') ||
         location.pathname.includes('allStudent') ||
 
-        location.pathname.includes('Payment')  ||
+        location.pathname.includes('Payment') ||
         location.pathname.includes('enterPhone') ||
         location.pathname.includes('enterotp') ||
         location.pathname.includes('studentEdit') ||
 
-        location.pathname.includes('adminTeachers') ||       
-        location.pathname.includes('adminTeachersEdit')  ||      
-        location.pathname.includes('adminTeacherAdd') ||    
-        
+        location.pathname.includes('adminTeachers') ||
+        location.pathname.includes('adminTeachersEdit') ||
+        location.pathname.includes('adminTeacherAdd') ||
+
         location.pathname.includes('adminEmployees') ||
         location.pathname.includes('adminEmployeesEdit') ||
         location.pathname.includes('adminEmployeesAdd') ||
 
-        location.pathname.includes('adminStudentAdd')  || 
+        location.pathname.includes('adminStudentAdd') ||
         location.pathname.includes('adminStudentEdit') ||
         location.pathname.includes('adminStudentDetails') ||
         location.pathname.includes('studentIdCard') ||
-        
-        
-        location.pathname.includes('adminNotices') ||      
-        location.pathname.includes('adminNoticesAdd') ||      
-        location.pathname.includes('adminNoticesEdit')  || 
 
-        location.pathname.includes('adminEvent')  || 
 
-        location.pathname.includes('adminRoutines')   ||    
-        location.pathname.includes('adminRoutineAdd')  ||     
-        location.pathname.includes('adminRoutineEdit') ||  
+        location.pathname.includes('adminNotices') ||
+        location.pathname.includes('adminNoticesAdd') ||
+        location.pathname.includes('adminNoticesEdit') ||
 
-        location.pathname.includes('adminSyllabus')   ||    
-        location.pathname.includes('adminSyllabusAdd') ||      
-        location.pathname.includes('adminSyllabusEdit') ||   
-           
-        location.pathname.includes('pendingPayment')  ||     
-        location.pathname.includes('approvedPayment')  || 
+        location.pathname.includes('adminEvent') ||
 
-        location.pathname.includes('unpaidStudent') ||    
+        location.pathname.includes('adminRoutines') ||
+        location.pathname.includes('adminRoutineAdd') ||
+        location.pathname.includes('adminRoutineEdit') ||
 
-        location.pathname.includes('invoice') ||    
+        location.pathname.includes('adminSyllabus') ||
+        location.pathname.includes('adminSyllabusAdd') ||
+        location.pathname.includes('adminSyllabusEdit') ||
 
-        location.pathname.includes('paymentHistory')     || 
-        location.pathname.includes('/adminAdmissionMng')   ||    
-        location.pathname.includes('/adminClassMng')      ||
-        location.pathname.includes('/adminExamination')   ||    
-        location.pathname.includes('/adminAccountsMng')    || 
-        location.pathname.includes('/adminPrintableResult')       
+        location.pathname.includes('pendingPayment') ||
+        location.pathname.includes('approvedPayment') ||
+
+        location.pathname.includes('unpaidStudent') ||
+
+        location.pathname.includes('invoice') ||
+
+        location.pathname.includes('paymentHistory') ||
+        location.pathname.includes('/adminAdmissionMng') ||
+        location.pathname.includes('/adminClassMng') ||
+        location.pathname.includes('/adminExamination') ||
+        location.pathname.includes('/adminAccountsMng') ||
+        location.pathname.includes('/adminPrintableResult')
 
     return (
         <div>
--- src/utilies/config.js
+++ src/utilies/config.js
@@ -1,5 +1,7 @@
- const coreUrl = "https://schoolbackend.softplatoon.com/api/";
+const coreUrl = "https://schoolbackend.softplatoon.com/api/";
 
-export function baseUrl(route){
-    return `${coreUrl}${route}` ;
+export function baseUrl(route) {
+    return `${coreUrl}${route}`;
 }
+
+
